ecf81cf207d659a22a1a77ece16f296f10c65da8
joegilberto <joekgilberto@gmail.com>
2023-10-12 13:03:22 -0400

    refactored main grids from flex display to grid

--- frontend/src/components/Grid/Grid.jsx
+++ frontend/src/components/Grid/Grid.jsx
@@ -5,18 +5,19 @@ import CoordinatesRow from '../CoordinatesRow/CoordinatesRow';
 
 export default function Grid({ owner }) {
 
-    const gridSize = [0,1,2,3,4,5]
+    const gridNums = ["one","two","three","four","five","six"]
+    const gridLetters = ["a","b","c","d","e","f"]
     return (
         <div className='Grid'>
             {owner === 'enemy' ? <CoordinatesRow /> : null}
             <div className='grid-border'>
             <div className={`grid-squares ${owner}`}>
                 <div className='row'>
-                    {gridSize.map((gridI, i) => {
+                    {gridLetters.map((letter, i) => {
                         return (
                             <div key={i} className='column'>
-                                {gridSize.map((gridII, j) => {
-                                    return <Square key={j} owner={owner} row={i} column={j} />
+                                {gridNums.map((num, j) => {
+                                    return <Square key={j} owner={owner} row={letter} column={num} idx={j} />
                                 })}
                             </div>
                         )
--- frontend/src/components/Square/Square.jsx
+++ frontend/src/components/Square/Square.jsx
@@ -1,9 +1,9 @@
 import './Square.css';
 
-export default function Square({owner, row, column, text}) {
+export default function Square({owner, row, column, idx, text}) {
 
   return (
-    <div className={`Square ${owner==='grave-ally'||owner==='grave-enemy'?'grave':owner} ${String.fromCharCode(row + 65).toLowerCase()} ${String.fromCharCode(90-column).toLowerCase()}`} style={{animation: `shine-${owner==='grave-ally'||owner==='grave-enemy'?'grave':owner} 1s forwards ${column-(.9*column)+(owner==='ally'?.6:owner==='grave-enemy'?.7:0)}s`}} >
+    <div className={`Square ${owner==='grave-ally'||owner==='grave-enemy'?'grave':owner} ${row} ${column}`} style={{animation: `shine-${owner==='grave-ally'||owner==='grave-enemy'?'grave':owner} 1s forwards ${idx-(.9*idx)+(owner==='ally'?.6:owner==='grave-enemy'?.7:0)}s`}} >
         {text?<p>{text}</p>:null}
     </div>
   );
